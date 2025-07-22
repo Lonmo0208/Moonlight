341cd6e8b93ff56a828ba7701c54e6be9a77f10e
Lonmo <95102577+Lonmo0208@users.noreply.github.com>
2025-07-23 04:37:23 +0800

    Update PoiMixin.java

--- common/src/main/java/net/mehvahdjukaar/moonlight/core/mixins/PoiMixin.java
+++ common/src/main/java/net/mehvahdjukaar/moonlight/core/mixins/PoiMixin.java
@@ -1,9 +1,10 @@
 package net.mehvahdjukaar.moonlight.core.mixins;
 
-import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
-import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
+import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
+import com.llamalad7.mixinextras.sugar.Local;
 import net.minecraft.world.entity.ai.village.poi.PoiType;
 import net.minecraft.world.level.block.state.BlockState;
+import net.neoforged.neoforge.common.world.poi.PoiStateSet;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 
@@ -12,11 +13,29 @@ import java.util.HashSet;
 import java.util.Set;
 
 @Mixin(value = PoiType.class, priority = 2000)
-public class PoiMixin {
+public abstract class PoiMixin {
 
-    @WrapOperation(method = "<init>",
-            at = @At(value = "INVOKE", target = "Ljava/util/Set;copyOf(Ljava/util/Collection;)Ljava/util/Set;"))
-    private Set<BlockState> moonlight$makePOIMutable(Collection<BlockState> coll, Operation<Set<BlockState>> original) {
-        return new HashSet<>(original.call(coll));
+    @ModifyExpressionValue(
+        method = "<init>",
+        at = @At(
+            value = "INVOKE",
+            target = "Ljava/util/Set;copyOf(Ljava/util/Collection;)Ljava/util/Set;"
+        )
+    )
+    private Set<BlockState> moonlight$wrapWithMutableSet(
+        Set<BlockState> original,
+        @Local(argsOnly = true) Collection<BlockState> states
+    ) {
+        HashSet<BlockState> mutableSet = new HashSet<>(states);
+        
+        if (original instanceof PoiStateSet) {
+            return original;
+        }
+        
+        return new PoiStateSet(mutableSet) {
+            public Set<BlockState> getBackingSet() {
+                return this.backingSet;
+            }
+        };
     }
 }
